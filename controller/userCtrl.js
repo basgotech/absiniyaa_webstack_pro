200b010ad1646c25fc337a2c507b9933b7e7aae2
Basliel Tegegn <basgolilo5@gmail.com>
2024-09-19 14:53:17 +0300

    fix

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { genererateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
@@ -20,7 +21,14 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
     //check if user exists or not in db
     const findUser = await User.findOne({ email });
     if(findUser && await findUser.isPasswordMatched(password)){
-     res.json(findUser);
+     res.json({
+        _id: findUser?._id,
+        firstname: findUser?.firstname,
+        lastname: findUser?.lastname,
+        email: findUser?.email,
+        mobile: findUser?.mobile,
+        token:genererateToken(findUser?._id),
+     });
     }else{
         throw new Error("Invalid Credentials");
     }
