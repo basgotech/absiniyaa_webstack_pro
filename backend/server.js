8a1628185325369c48df2339a04181c91a746dff
Basliel Tegegn <basgolilo5@gmail.com>
2024-09-20 18:22:44 +0300

    db connect

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
-
 app.get('/api/products', (req, res) => {
     res.send(data.products)
 });
-
 app.get('/api/products/slug/:slug', (req, res) => {
     const product = data.products.find(x => x.slug === req.params.slug);
     if(product) {
