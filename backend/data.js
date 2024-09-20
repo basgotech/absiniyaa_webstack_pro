3bac9b685f928b9473dacdf5702e6dc727be138a
Basliel Tegegn <basgolilo5@gmail.com>
2024-09-20 10:37:49 +0300

    add

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Samsung ultra',
+            slug: 'Samsung-max',
+            category: 'cellphone',
+            image: '/images/p1.jpg',
+            price: 1200,
+            countInStock: 10,
+            brand: 'Sumsung',
+            rating:4.5,
+            numreviewa:10,
+            decription: 'high quality Mobile'
+        },
+        {
+            name: 'zte ultra',
+            slug: 'zte-max',
+            category: 'cellphone',
+            image: '/images/p2.jpg',
+            price: 1200,
+            countInStock: 10,
+            brand: 'zte',
+            rating:4.5,
+            numreviewa:10,
+            decription: 'high quality mobile'
+        },
+        {
+            name: 'Iphone ultra',
+            slug: 'Iphone-max',
+            category: 'cellphone',
+            image: '/images/p3.jpg',
+            price: 1200,
+            countInStock: 10,
+            brand: 'Apple',
+            rating:4.5,
+            numreviewa:10,
+            decription: 'high quality Mobile camera'
+        },
+        {
+            name: 'Huwawei ultra',
+            slug: 'Huwawei-max',
+            category: 'cellphone',
+            image: '/images/p4.jpg',
+            price: 1200,
+            countInStock: 10,
+            brand: 'Huwawei',
+            rating:4.5,
+            numreviewa:10,
+            decription: 'high quality Mobile camera'
+        },
+    ],
+};
+
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-       
-       <a href="/">Absiniyaa</a>
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <Navbar.Brand>Absiniyaa</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
-        list products
+        <Routes>
+          <Route path='/product/:slug' element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen/>} />
+        </Routes>
+        
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
